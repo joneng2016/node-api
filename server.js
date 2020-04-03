b14f5d42e621639520f0ac30d059dd5cc39dcde4
joneng2016 <jonathan_samara@hotmail.com>
2020-04-02 21:14:46 -0300

    video criando model

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-const express = require('express');
+var express = require('express');
+var mongoose  = require('mongoose');
+var requireDir = require('require-dir');
 
+// Iniciando o APP
 const app = express();
+
+// Iniciando o Banco de Dados
+mongoose.connect("mongodb://localhost:27017/nodeapi",{useNewUrlParser:true,useUnifiedTopology:true})
+
 
 app.get("/",(req,res) => {
     res.send("Hello Word");
 });
+requireDir("./src/models");
+
 
 app.listen(3001);
