d74e935c815a7cc0d3c85e029bca6624fac3ba83
joneng2016 <jonathan_samara@hotmail.com>
2020-04-03 23:52:55 -0300

    feito até a instalacao do insomnia

--- server.js
+++ server.js
@@ -7,12 +7,9 @@ const app = express();
 
 // Iniciando o Banco de Dados
 mongoose.connect("mongodb://localhost:27017/nodeapi",{useNewUrlParser:true,useUnifiedTopology:true})
-
-
-app.get("/",(req,res) => {
-    res.send("Hello Word");
-});
 requireDir("./src/models");
 
+// Rotas
+app.use("/api",require("./src/routes"));
 
 app.listen(3001);
